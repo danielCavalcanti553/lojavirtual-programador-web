6ca4688aba09678519b4fdce0ba8d66ff6079183
Programador Web 2019.2 <16124512019.2@senacrjedu.local>
2019-11-28 17:28:28 -0300

    Composer e PHPUnit

---
+++ src/Model/Cliente.php
@@ -0,0 +1,69 @@
+<?php
+namespace LOJA\Model;
+use LOJA\includes\Util;
+class Cliente{
+
+    private $id;
+    private $nome;
+    private $telefone;
+    private $email;
+    private $cpf;
+
+    public function __construct(){
+    }
+
+    public function getId(){
+		return $this->id;
+	}
+
+	public function setId($id){
+		$this->id = $id;
+	}
+
+	public function getNome(){
+		return $this->nome;
+	}
+
+	public function setNome($nome){
+		if($nome==="") throw new \Exception('Nome Inválido');
+		
+		$this->nome = $nome;
+	}
+
+	public function getTelefone(){
+		return $this->telefone;
+	}
+
+	public function setTelefone($telefone){
+		$this->telefone = $telefone;
+	}
+
+	public function getEmail(){
+		
+		return $this->email;
+	}
+
+	public function setEmail($email){
+		if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+			$this->email = $email;
+		}else{
+			throw new \Exception("E-mail Inválido");
+		}
+	}
+
+	public function getCpf(){
+		return $this->cpf;
+	}
+
+	public function setCpf($cpf){
+		if(Util::validaCPF($cpf)){
+			throw new \Exception("CPF Inválido");
+		}else{
+
+		$this->cpf = $cpf;
+	}
+		
+	}
+}
+
+?>
